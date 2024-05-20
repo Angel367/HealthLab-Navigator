74e4c7bf6a3bebe2573a86294b608840bc67d52f
13melisa13 <isaeva.a.v@edu.mirea.ru>
2024-05-20 16:39:56 +0300

    laboratories styling

--- healthlab_navigator_frontend/src/agregator/Laboratories.js
+++ healthlab_navigator_frontend/src/agregator/Laboratories.js
@@ -3,17 +3,51 @@
 function Laboratories() {
     return (
         <main>
-            {/*<h1>Лаборатории</h1>*/}
-            {/*<FilterForm options={labList} setSelectedOption={setSelectedItems}*/}
-            {/*            labelName='name' valueName='id_laboratory'/>*/}
-            {/*<HolderAdv advList={advList}/>*/}
-            {/*<div className="laboratory cards">*/}
-            {/*    {selectedItems.map((lab, index) => {*/}
-            {/*        return (*/}
-            {/*           <CardAnalysis key={index} {...lab}/>*/}
-            {/*        );*/}
-            {/*    })}*/}
-            {/*</div>*/}
+            <div className="container">
+                <h1>Лаборатории</h1>
+                <div className="row">
+                    <div className="col-md-3">
+                        <div className="card">
+                            <img src="https://via.placeholder.com/150" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Название лаборатории</h5>
+                                <p className="card-text">Описание лаборатории</p>
+                                <a href="#" className="btn btn-primary">Перейти на сайт</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="card">
+                            <img src="https://via.placeholder.com/150" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Название лаборатории</h5>
+                                <p className="card-text">Описание лаборатории</p>
+                                <a href="#" className="btn btn-primary">Перейти на сайт</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="card">
+                            <img src="https://via.placeholder.com/150" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Название лаборатории</h5>
+                                <p className="card-text">Описание лаборатории</p>
+                                <a href="#" className="btn btn-primary">Перейти на сайт</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="card">
+                            <img src="https://via.placeholder.com/150" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Название лаборатории</h5>
+                                <p className="card-text">Описание лаборатории</p>
+                                <a href="#" className="btn btn-primary">Перейти на сайт</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
